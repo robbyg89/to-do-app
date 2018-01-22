ef6797c86b9f7bec89c3a39d16495e4af6c0ad07
Robby <robbyg89@gmail.com>
2018-01-22 23:07:57 +0000

    created basic app

--- to-do-app/app.js
+++ to-do-app/app.js
@@ -1,36 +1,3 @@
-function onReady(){
-     const addToDoForm = document.getElementById('addToDoForm');
-     const newToDoText = document.getElementById('newToDoText');
-     const toDoList = document.getElementById('toDoList');
-     addToDoForm.addEventListener('submit', () => {
-        event.preventDefault();
-        //get the text
-        let title = newToDoText.value;
-       
-        //create a new li 
-        let newLi = document.createElement('li');
-       
-        //create a new input
-        let checkbox = document.createElement('input');
-       
-        //set the inputs type to checkbox
-        checkbox.type = "checkbox";
-       
-        //set the title
-        newLi.textContent = title;
-       
-        //attatch the checkbox to the Li
-        newLi.appendChild(checkbox);
-       
-        //attatch the li to the ul
-        toDoList.appendChild(newLi);
-       
-        //empty the input
-        newToDoText.value = '';
-     });
-         
-    }
-
-window.onload = function() {
-    onReady();
+window.onload = function(){
+    alert("The window has loaded!");
 };
