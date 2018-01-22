daccc20235c866aa8231ab79ca530fb08bb5b769
Robby <robbyg89@gmail.com>
2018-01-22 23:41:09 +0000

    debug app.js

--- to-do-app/app.js
+++ to-do-app/app.js
@@ -1,3 +1,34 @@
-window.onload = function(){
-    alert("The window has loaded!");
-};
+function onReady(){
+    const addtoDoForm = document.getElementById('addtoDoForm');
+    const newToDoText = document.getElementById('newToDoText');
+    const toDoList = document.getElementById('toDoList');
+    
+    addtoDoForm.addEventListener('submit', event => {
+        event.preventDefault();
+        
+        // get the text
+        let title = newToDoText.value;
+        
+        // create new li
+        let newLi = document.createElement('li');
+        
+        // create a new input
+        let checkbox = document.createElement('input');
+        
+        //set the input's type to checkbox
+        checkbox.type = "checkbox";
+        
+        // set the title
+        newLi.textContent = title;
+        
+        //attach the checkbox to the li
+        newLi.appendChild(checkbox);
+        
+        //attach the li to the ul
+        toDoList.appendChild(newLi);
+        
+        //empty the input
+        newToDoText.value = '';
+    });
+}
+
