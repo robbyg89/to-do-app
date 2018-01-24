d88a302afc5ee9de5253e2991139a3bf670c42ee
Robby <robbyg89@gmail.com>
2018-01-24 02:43:31 +0000

    more functionality

--- to-do-app/app.js
+++ to-do-app/app.js
@@ -1,9 +1,9 @@
 function onReady(){
-    const addtoDoForm = document.getElementById('addtoDoForm');
-    const newToDoText = document.getElementById('newToDoText');
-    const toDoList = document.getElementById('toDoList');
-    
-    addtoDoForm.addEventListener('submit', event => {
+    const addToDoForm = document.getElementById('addTodoForm');
+    const newToDoText = document.getElementById('newTodoText');
+    const toDoList = document.getElementById('todoList');
+    console.log(addToDoForm);
+    addToDoForm.addEventListener('submit', event => {
         event.preventDefault();
         
         // get the text
@@ -15,20 +15,28 @@ function onReady(){
         // create a new input
         let checkbox = document.createElement('input');
         
-        //set the input's type to checkbox
+        // set the input's type to checkbox
         checkbox.type = "checkbox";
         
         // set the title
         newLi.textContent = title;
         
-        //attach the checkbox to the li
+        // attach the checkbox to the li
         newLi.appendChild(checkbox);
         
-        //attach the li to the ul
+        // attach the li to the ul
         toDoList.appendChild(newLi);
         
-        //empty the input
+        // empty the input
         newToDoText.value = '';
+        
+        // delete To-Do
+        
     });
 }
+
+
+window.onload = function(){
+    onReady();
+};
 
